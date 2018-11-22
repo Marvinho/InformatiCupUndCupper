7db8feca6db68bd04f71165f49b22eeb155fc28a
Marvinho <18325331+Marvinho@users.noreply.github.com>
2018-11-22 09:51:25 +0100

    bugfix generateimage

--- own_cnn_attempt/generateimage.py
+++ own_cnn_attempt/generateimage.py
@@ -27,7 +27,7 @@ def createImage(random, color = None):
     plt.show()    
     print("saving image...")
     date_string = time.strftime("%Y-%m-%d-%H_%M")
-    test_image.save("../Images/originals/orig_image_{}_color_{}.png".format(date_string, color))
+    test_image.save("./Images/originals/orig_image_{}_color_{}.png".format(date_string, color))
 
 
 if __name__ == "__main__":
